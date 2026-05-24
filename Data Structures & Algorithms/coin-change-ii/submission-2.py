a0f07cd6aeefa573dc1819137fde8e044c64326a
Swayam Swaroop Mishra <44289328+Swayam595@users.noreply.github.com>
2026-05-25 00:07:23 +0530

    Add: coin-change-ii - submission-2

---
+++ Data Structures & Algorithms/coin-change-ii/submission-2.py
@@ -0,0 +1,58 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        # return self.__recursive(0, amount, coins)
+        # return self.__topDown(amount, coins)
+        return self.__bottomUp(amount, coins)
+
+    def __recursive(self, i: int, amount: int, coins: List[int]) -> int:
+        if amount == 0:
+            return 1
+        
+        if i == len(coins) or amount < 0:
+            return 0
+        
+        take = self.__recursive(i, amount - coins[i], coins)
+        skip = self.__recursive(i + 1, amount, coins)
+
+        return take + skip
+
+    def __topDown(self, amount: int, coins: list[int]) -> int:
+        N = len(coins)
+        M = amount
+        memo = [[0] * (M + 1) for _ in range(N + 1)]
+
+        return self.__topDownHelper(0, amount, coins, memo)
+
+    def __topDownHelper(self, i: int, amount: int, coins: List[int], memo: List[List[int]]) -> int:
+        if amount == 0:
+            return 1
+        
+        if i == len(coins) or amount < 0:
+            return 0
+        
+        if memo[i][amount] != 0:
+            return memo[i][amount]
+        
+        take = self.__topDownHelper(i, amount - coins[i], coins, memo)
+        skip = self.__topDownHelper(i + 1, amount, coins, memo)
+
+        memo[i][amount] = take + skip
+
+        return memo[i][amount]
+
+    def __bottomUp(self, amount: int, coins: List[int]) -> int:
+        N = len(coins)
+        M = amount
+
+        dp = [[0] * (M + 1) for _ in range(N + 1)]
+
+        for i in range(N - 1, -1, -1):
+            for j in range(M + 1):
+                if j == 0:
+                    dp[i][j] = 1
+                else:
+                    take = dp[i][j - coins[i]] if j - coins[i] >= 0 else 0
+                    skip = dp[i + 1][j]
+                    dp[i][j] = take + skip
+        
+        return dp[0][M]
