53290dc97f4eea26a096a81f1461464dc928dbfb
Swayam Swaroop Mishra <44289328+Swayam595@users.noreply.github.com>
2026-06-16 07:24:44 +0530

    Add: longest-common-subsequence - submission-6

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-6.py
@@ -0,0 +1,47 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        # return self.__recursive(text1, text2, 0, 0)
+        return self.__topDown(text1, text2)
+    
+    # TC - O(2 ^ (N + M))
+    # SC - O(N + M)
+    def __recursive(self, s1: str, s2: str, i1: int, i2: int) -> int:
+        if i1 == len(s1) or i2 == len(s2):
+            return 0
+        
+        if s1[i1] == s2[i2]:
+            return 1 + self.__recursive(s1, s2, i1 + 1, i2 + 1)
+        else:
+            take_i1 = self.__recursive(s1, s2, i1, i2 + 1)
+            take_i2 = self.__recursive(s1, s2, i1 + 1, i2)
+
+            return max(take_i1, take_i2)
+
+    # TC - O(N * M)
+    # SC - O(N * M)
+    def __topDown(self, s1: str, s2: str) -> int:
+        N = len(s1)
+        M = len(s2)
+
+        cache = [[-1] * (M) for _ in range(N)]
+
+        return self.__topDownHelper(s1, s2, 0, 0, cache)
+    
+    # TC - O(N * M)
+    # SC - O(N + M)
+    def __topDownHelper(self, s1: str, s2: str, i1: int, i2: int, cache: List[List[int]]) -> int:
+        if i1 == len(s1) or i2 == len(s2):
+            return 0
+        
+        if cache[i1][i2] != -1:
+            return cache[i1][i2]
+        
+        if s1[i1] == s2[i2]:
+            cache[i1][i2] = 1 + self.__topDownHelper(s1, s2, i1 + 1, i2 + 1, cache)
+        else:
+            take_i1 = self.__topDownHelper(s1, s2, i1, i2 + 1, cache)
+            take_i2 = self.__topDownHelper(s1, s2, i1 + 1, i2, cache)
+
+            cache[i1][i2] = max(take_i1, take_i2)
+        
+        return cache[i1][i2]
