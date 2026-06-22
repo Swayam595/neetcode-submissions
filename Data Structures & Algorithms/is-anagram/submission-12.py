d30aed4056b77c04d343f420a288e9662f6d8754
Swayam Swaroop Mishra <44289328+Swayam595@users.noreply.github.com>
2026-06-22 22:47:54 +0530

    Add: is-anagram - submission-12

---
+++ Data Structures & Algorithms/is-anagram/submission-12.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        char_map = self.__getCharMap(s)
+
+        for char in t:
+            if char not in char_map:
+                return False
+            
+            char_map[char] -= 1
+
+            if char_map[char] == 0:
+                del char_map[char]
+            
+        return True if len(char_map) == 0 else False
+
+    def __getCharMap(self, s: str):
+        char_map = dict()
+
+        for char in s:
+            char_map[char] = char_map.get(char, 0) + 1
+        
+        return char_map
