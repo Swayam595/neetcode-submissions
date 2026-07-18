a870ebb3b4d8967334b59678097f97bc252cc171
Swayam Swaroop Mishra <44289328+Swayam595@users.noreply.github.com>
2026-07-18 23:42:39 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        parentheses = {
+            ")": "(",
+            "}": "{",
+            "]": "["
+        }
+
+        stack = []
+
+        for char in s:
+            if char not in parentheses:
+                stack.append(char)
+            else:
+                if len(stack) == 0 or stack[-1] != parentheses[char]:
+                    return False
+                stack.pop()
+
+        return len(stack) == 0 
