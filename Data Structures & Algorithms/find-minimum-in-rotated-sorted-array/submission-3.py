6c3e8afbada0f8ef1e93705009c6714feee1197f
Swayam Swaroop Mishra <44289328+Swayam595@users.noreply.github.com>
2026-07-19 00:05:50 +0530

    Add: find-minimum-in-rotated-sorted-array - submission-3

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        # return self.__findMinBruteForce(nums)
+        return self.__findMinBinarySerach(nums)
+    
+    # TC - O(N)
+    # SC - O(1)
+    def __findMinBruteForce(self, nums: List[int]) -> int:
+        min_value = float('inf')
+        for num in nums:
+            min_value = min(min_value, num)
+        
+        return min_value
+    
+    # TC - O(logN)
+    # SC - O(1)
+    def __findMinBinarySerach(self, nums: List[int]) -> int:
+        l = 0
+        r = len(nums) - 1
+
+        while l < r:
+            mid = l + (r - l) // 2
+
+            if nums[mid] < nums[r]:
+                r = mid
+            else:
+                l = mid + 1
+        
+        return nums[l]
