3c96ea7ecf2df98e402c708f02bfd84fb74e40d1
Swayam Swaroop Mishra <44289328+Swayam595@users.noreply.github.com>
2026-07-18 23:35:23 +0530

    Add: minimum-window-with-characters - submission-4

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-4.py
@@ -0,0 +1,71 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(s) < len(t) or len(t) == 0:
+            return ""
+
+        return self.__minWindowBruteForce(s, t)
+        # return self.__minWindwoSlidingWindow(s, t)
+    
+    # TC - O(N^2 * M)
+    # SC - O(M)
+    # N -> len of s
+    # M -> len of t
+    def __minWindowBruteForce(self, s: str, t: str) -> str:
+        t_char_space = dict()
+
+        for char in t:
+            t_char_space[char] = 1 + t_char_space.get(char, 0)
+
+        ans = [-1, -1]
+        min_len = float('inf')
+
+        for l in range(len(s)):
+            s_char_space = dict()
+            for r in range(l, len(s)):
+                s_char_space[s[r]] = 1 + s_char_space.get(s[r], 0)
+
+                if self.__validateMinWindow(t_char_space, s_char_space) and (r - l + 1) < min_len:
+                    min_len = r - l + 1
+                    ans = [l, r]
+        
+        l, r = ans
+
+        return s[l : r + 1]
+
+    def __validateMinWindow(self, t_char_space: dict, s_char_space: dict) -> bool:
+        for char in t_char_space:
+            if t_char_space[char] > s_char_space.get(char, 0):
+                return False
+        return True
+
+    def __minWindwoSlidingWindow(self, s: str, t: str) -> str:
+        ans_index = [-1, -1]
+        min_len = float('inf')
+
+        t_char_space = dict()
+        have = 0
+        need = len(t)
+
+        for char in t:
+            t_char_space[char] = 1 + t_char_space.get(char, 0)
+
+        window = dict()
+        l = 0
+        for r in range(len(s)):
+            char = s[r]
+            window[char] = 1 + window.get(char, 0)
+
+            if char in t_char_space and window[char] == t_char_space[char]:
+                have += 1
+            
+            while have == need:
+                if (r - l + 1) < min_len:
+                    min_len = r - l + 1
+                    ans_index = [l, r]
+                
+                window[s[l]] -= 1
+                if s[l] in t_char_space and window[s[l]] < t_char_space[s[l]]:
+                    have -= 1
+                l += 1
+        l, r = ans_index
+        return s[l : r + 1]
