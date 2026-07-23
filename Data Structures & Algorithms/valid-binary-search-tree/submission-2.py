9542e3e08dcff519f452399a0a9212d5628eaec8
Swayam Swaroop Mishra <44289328+Swayam595@users.noreply.github.com>
2026-07-23 08:14:35 +0530

    Add: valid-binary-search-tree - submission-2

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-2.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True
+
+        q = deque()
+        
+        q.append([root, -float('inf'), float('inf')])
+
+        while len(q) > 0:
+            node, left, right = q.popleft()
+
+            if not (left < node.val and node.val < right):
+                return False
+
+            if node.left is not None:
+                q.append([node.left, left, node.val])
+            
+            if node.right is not None:
+                q.append([node.right, node.val, right])
+
+        return True
