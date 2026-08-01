aedc2c6ea687888dc56265c2d0f74a18d00c0c51
Swayam Swaroop Mishra <44289328+Swayam595@users.noreply.github.com>
2026-08-01 20:55:49 +0530

    Add: house-robber - submission-17

---
+++ Data Structures & Algorithms/house-robber/submission-17.py
@@ -0,0 +1,81 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # return self.__rob_recursive(0, nums)
+        # return self.__rob_top_down(nums)
+        # return self.__rob_bottom_up(nums)
+        return self.__rob_bottom_up_optimized(nums)
+
+    # TC -> O(2 ^ N)
+    # SC -> O(N)
+    # N -> len of the nums
+    def __rob_recursive(self, i: int, nums: List[int]) -> int:
+        if i >= len(nums):
+            return 0
+        
+        take = nums[i] + self.__rob_recursive(i + 2, nums)
+        skip = self.__rob_recursive(i + 1, nums)
+
+        return max(take, skip)
+
+    # TC -> O(N)
+    # SC -> O(N)
+    # N -> len of the nums
+    def __rob_top_down(self, nums: List[int]) -> int:
+        N = len(nums)
+        cache = [-1] * (N + 1)
+
+        return self.__rob_top_down_helper(0, nums, cache)
+
+
+    def __rob_top_down_helper(self, i: int, nums: List[int], cache:List[int]) -> int:
+        if i >= len(nums):
+            return 0
+
+        if cache[i] != -1:
+            return cache[i]
+
+        take = nums[i] + self.__rob_top_down_helper(i + 2, nums, cache)
+        skip = self.__rob_top_down_helper(i + 1, nums, cache)
+
+        cache[i] = max(take, skip)
+
+        return cache[i]
+
+    # TC -> O(N)
+    # SC -> O(N)
+    # N -> len of nums
+    def __rob_bottom_up(self, nums: List[int]) -> int:
+        N = len(nums)
+        dp = [0] * (N + 2)
+
+        for i in range(N - 1, -1, -1):
+            take = nums[i] + dp[i + 2]
+            skip = dp[i + 1]
+            dp[i] = max(take, skip)
+        
+        return dp[0]
+
+    # TC -> O(N)
+    # SC -> O(1)
+    # N -> len of nums
+    def __rob_bottom_up_optimized(self, nums: List[int]) -> int:
+        N = len(nums)
+
+        if N <= 2:
+            return max(nums)
+
+        m1 = nums[0]
+        m2 = max(nums[0], nums[1])
+        
+        max_money = 0
+
+        for i in range(2, N):
+            take = nums[i] + m1
+            skip = m2
+
+            max_money = max(take, skip)
+
+            m1 = skip
+            m2 = max_money
+
+        return max_money
