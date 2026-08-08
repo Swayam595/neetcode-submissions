e653bdcbb00a9d571c2233e95606c567d9378e39
Swayam Swaroop Mishra <44289328+Swayam595@users.noreply.github.com>
2026-08-09 00:07:14 +0530

    Add: palindromic-substrings - submission-1

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        max_palindromes = 0
+
+        for i in range(len(s)):
+            max_palindromes += self.__count_palindormeic_sub_strings(i, i, s)
+            max_palindromes += self.__count_palindormeic_sub_strings(i, i + 1, s)
+
+        return max_palindromes
+
+    def __count_palindormeic_sub_strings(self, l: int, r: int, s: str) -> int:
+        no_of_palindormes = 0
+
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            no_of_palindormes += 1
+            l -= 1
+            r += 1
+        
+        return no_of_palindormes
