d766398dd8349f82a9ca9856ce00b85b1d746885
Swayam Swaroop Mishra <44289328+Swayam595@users.noreply.github.com>
2026-08-17 08:48:10 +0530

    Add: rotate-matrix - submission-2

---
+++ Data Structures & Algorithms/rotate-matrix/submission-2.py
@@ -0,0 +1,46 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        # self.__brute_force(matrix)
+        self.__rotate_inplace(matrix)
+
+    # TC -> O(N ^ 2)
+    # SC -> O(1)
+    # N -> len of matrix
+    def __rotate_inplace(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+
+        l = 0
+        r = n - 1
+
+        while l < r:
+            for i in range(r - l):
+                top = l 
+                bottom = r
+
+                top_left = matrix[top][l + i]
+
+                matrix[top][l + i] = matrix[bottom - i][l]
+
+                matrix[bottom - i][l] = matrix[bottom][r - i]
+
+                matrix[bottom][r - i] = matrix[top + i][r]
+
+                matrix[top + i][r] = top_left
+
+            r -= 1
+            l += 1         
+
+    # TC -> O(N ^ 2)
+    # SC -> O(N ^ 2)
+    # N -> len of matrix
+    def __brute_force(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        ans = []
+        for j in range(n):
+            column = []
+            for i in range(n - 1, -1, -1):
+                column.append(matrix[i][j])
+            ans.append(column)
+
+        for i in range(n):
+            matrix[i] = ans[i]
