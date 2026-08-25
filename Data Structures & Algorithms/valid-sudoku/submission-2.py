c1cc48379273f3ffdff236fccbf85dcbe4e07a97
Swayam Swaroop Mishra <44289328+Swayam595@users.noreply.github.com>
2026-08-25 06:43:08 +0530

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,38 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        return self.__is_valid_sudoku(board)
+
+    # TC -> O(1)
+    # SC -> O(1)
+    def __is_valid_sudoku(self, board: List[List[str]]) -> bool:
+        for row in range(9):
+            seen = set()
+            for i in range(9):
+                if board[row][i] == '.':
+                    continue
+                if board[row][i] in seen:
+                    return False
+                seen.add(board[row][i])
+
+        for col in range(9):
+            seen = set()
+            for i in range(9):
+                if board[i][col] == ".":
+                    continue
+                if board[i][col] in seen:
+                    return False
+                seen.add(board[i][col])
+
+        for square in range(9):
+            seen = set()
+            for i in range(3):
+                for j in range(3):
+                    row = (square // 3) * 3 + i
+                    col = (square % 3) * 3 + j
+                    if board[row][col] == ".":
+                        continue
+                    if board[row][col] in seen:
+                        return False
+                    seen.add(board[row][col])
+        
+        return True
