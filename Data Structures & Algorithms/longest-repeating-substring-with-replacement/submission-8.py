bae6250660c879009e53f6366ede3f3cecbffaac
Swayam Swaroop Mishra <44289328+Swayam595@users.noreply.github.com>
2026-08-26 07:28:55 +0530

    Add: longest-repeating-substring-with-replacement - submission-8

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-8.py
@@ -0,0 +1,50 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # return self.__character_replacement_brute_force(s, k)
+        return self.__character_replacement_sliding_window(s, k)
+
+    # TC -> O(N)
+    # SC -> O(26)
+    # N -> Len of s
+    def __character_replacement_sliding_window(self, s: str, k: int) -> int:
+        char_set = set()
+        max_len = 0
+        for char in s:
+            char_set.add(char)
+
+        for char in char_set:
+            count = 0
+            l = 0
+            for r in range(len(s)):
+                if s[r] == char:
+                    count += 1
+                
+                while r - l + 1 - count > k:
+                    if s[l] == char:
+                        count -= 1
+                    l += 1
+                
+                max_len = max(max_len, r - l + 1)
+        
+        return max_len
+
+    # TC -> O(N ^ 2)
+    # SC -> O(1)
+    # N -> Len of s
+    def __character_replacement_brute_force(self, s: str, k: int) -> int:
+        max_len = 0
+        
+        for i, char in enumerate(s):
+            replacement_remaining = k
+            curr_len = 1
+            for j in range(i + 1, len(s)):
+                if char == s[j]:
+                    curr_len += 1
+                elif replacement_remaining > 0:
+                    replacement_remaining -= 1
+                    curr_len += 1
+                else:
+                    break
+                max_len = max(max_len, curr_len)
+        
+        return max_len
