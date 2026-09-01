4078b2f18eb573fd65a4f44b200e2defbea0fb1d
Swayam Swaroop Mishra <44289328+Swayam595@users.noreply.github.com>
2026-09-01 08:40:52 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    # TC -> O(N)
+    # SC -> O(N)
+    # N -> len of temperatures
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        monotonic_stack = []
+        ans = [0] * len(temperatures)
+
+        for i, temperature in enumerate(temperatures):
+            while len(monotonic_stack) > 0 and temperatures[monotonic_stack[-1]] < temperature:
+                top = monotonic_stack.pop()
+                ans[top] = i - top
+
+            monotonic_stack.append(i)
+        
+        return ans
